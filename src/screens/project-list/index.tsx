1384fb444d2e3c7982ea4a4d3b2598faf3cc2c5c
zhangli20080808 <791492434@qq.com>
2021-05-22 18:41:05 +0800

    first commit

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { SearchPanel } from "screens/project-list/search-panel";
+import { List } from "screens/project-list/list";
+import { useDebounce, useDocumentTitle } from "../../utils";
+import styled from "@emotion/styled";
+import { Typography } from "antd";
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
+import { useUrlQueryParam } from "utils/url";
+
+// 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
+// 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
+export const ProjectListScreen = () => {
+  // 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
+  // https://codesandbox.io/s/keen-wave-tlz9s?file=/src/App.js
+  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
+  const debouncedParam = useDebounce(param, 200);
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
+  useDocumentTitle("项目列表", false);
+
+  return (
+    <Container>
+      <h1>项目列表</h1>
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
+      {error ? (
+        <Typography.Text type={"danger"}>{error.message}</Typography.Text>
+      ) : null}
+      <List loading={isLoading} users={users || []} dataSource={list || []} />
+    </Container>
+  );
+};
+
+ProjectListScreen.whyDidYouRender = false;
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
---
+++ src/screens/project-list/list.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { User } from "screens/project-list/search-panel";
+import { Table } from "antd";
+import dayjs from "dayjs";
+import { TableProps } from "antd/es/table";
+// react-router 和 react-router-dom的关系，类似于 react 和 react-dom/react-native/react-vr...
+import { Link } from "react-router-dom";
+
+// TODO 把所有ID都改成number类型
+export interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  pin: boolean;
+  organization: string;
+  created: number;
+}
+
+interface ListProps extends TableProps<Project> {
+  users: User[];
+}
+
+export const List = ({ users, ...props }: ListProps) => {
+  return (
+    <Table
+      rowKey={"id"}
+      pagination={false}
+      columns={[
+        {
+          title: "名称",
+          sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
+        },
+        {
+          title: "部门",
+          dataIndex: "organization",
+        },
+        {
+          title: "负责人",
+          render(value, project) {
+            return (
+              <span>
+                {users.find((user) => user.id === project.personId)?.name ||
+                  "未知"}
+              </span>
+            );
+          },
+        },
+        {
+          title: "创建时间",
+          render(value, project) {
+            return (
+              <span>
+                {project.created
+                  ? dayjs(project.created).format("YYYY-MM-DD")
+                  : "无"}
+              </span>
+            );
+          },
+        },
+      ]}
+      {...props}
+    />
+  );
+};
